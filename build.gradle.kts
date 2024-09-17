d773178b88a9dcf175839d5f32256ab7107a9936
Carlos Ortiz <cortiz@jmpeax.com>
2024-09-16 21:26:53 -0600

    Update version to 0.1.4 and extend compatibility range
    Bump project version from 0.1.3 to 0.1.4. Adjust the IntelliJ plugin compatability range to include builds up to 243.*.

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "com.jmpeax"
-version = "0.1.3"
+version = "0.1.4"
 
 repositories {
     mavenCentral()
@@ -32,7 +32,7 @@ tasks {
 
     patchPluginXml {
         sinceBuild.set("232")
-        untilBuild.set("242.*")
+        untilBuild.set("243.*")
     }
 
     signPlugin {
